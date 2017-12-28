f960783ab5da2e58ea8e6a790b5e20accb2b696e
Maksim Lebedev <miskamvedebel@users.noreply.github.com>
2017-12-28 17:16:17 +0100

    Prediction Algorithms

---
+++ titanic_predictions.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import f1_score, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+#
+X_t, X_s, y_t, y_s = train_test_split(X_train, y_train, test_size = 0.25)
+#Random Forest classifier
+c_RT = RandomForestClassifier(n_estimators = 100, criterion='entropy')
+y_p = c_RT.fit(X_t, y_t).predict(X_s)
+f1_score(y_s, y_p)
+accuracy_score(y_s, y_p)
+#Logistic Regression
+c_LR = LogisticRegression()
+y_p = c_LR.fit(X_t, y_t).predict(X_s)
+f1_score(y_s, y_p)
+#Support Vector Machine
+c_SVC = LinearSVC()
+y_p = c_SVC.fit(X_t, y_t).predict(X_s)
+f1_score(y_s, y_p)
+#KNN
+c_KNN = KNeighborsClassifier(n_neighbors = 10, metric='minkowski')
+y_p = c_KNN.fit(X_t, y_t).predict(X_s)
+f1_score(y_s, y_p)
+#Decision Tree
+c_DT = DecisionTreeClassifier()
+y_p = c_DT.fit(X_t, y_t).predict(X_s)
+f1_score(y_s, y_p)
+accuracy_score(y_s, y_p)
+#Naive Bayes
+c_GNB = GaussianNB()
+y_p = c_GNB.fit(X_t, y_t).predict(X_s)
+f1_score(y_s, y_p)
+y_predict = c_RT.fit(X_train, y_train).predict(X_test)
+
+#Submission
+submission = pd.DataFrame({
+        "PassengerId": test["PassengerId"],
+        "Survived": y_predict
+    })
+submission.to_csv('C:\\Users\\maksim.lebedev\\Desktop\\MachineLearning\\Kaggle\\Titanic\\submission7.csv', 
+                  index = False)
